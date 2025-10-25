fbef9d30ba78eb53386903cd2b1ecf97ed409e87
sunsumabty <sansuma851@gmail.com>
2025-10-25 10:41:21 +0530

    2nd

--- app.py
+++ app.py
@@ -194,5 +194,9 @@ def cleanup_old_files():
             except:
                 pass
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
